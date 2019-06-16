0f383d7e60b5c147eb70db9d3f3b4fb106af413a
Takashi Masuyama <mamewotoko@gmail.com>
2019-06-16 16:23:34 +0900

    implement toolbar

--- lib/redmine_pandoc_formatter/helper.rb
+++ lib/redmine_pandoc_formatter/helper.rb
@@ -2,8 +2,10 @@ module RedminePandocFormatter
   module Helper
     unloadable
 
-    def wikitoolbar_for(field_id, dummy=nil)
-      nil
+    def wikitoolbar_for(field_id, preview_url = preview_text_path)
+          heads_for_wiki_formatter
+          url = "#{Redmine::Utils.relative_url_root}/help/#{current_language.to_s.downcase}/wiki_syntax_markdown.html"
+          javascript_tag("var wikiToolbar = new jsToolBar(document.getElementById('#{field_id}')); wikiToolbar.setHelpLink('#{escape_javascript url}'); wikiToolbar.setPreviewUrl('#{escape_javascript preview_url}'); wikiToolbar.draw();")
     end
 
     def initial_setup
@@ -15,7 +17,16 @@ module RedminePandocFormatter
     end
 
     def heads_for_wiki_formatter
-      nil
+      unless @heads_for_wiki_formatter_included
+        content_for :header_tags do
+          javascript_include_tag('jstoolbar/jstoolbar') +
+            javascript_include_tag('jstoolbar/markdown') +
+            javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
+            javascript_tag("var wikiImageMimeTypes = #{Redmine::MimeType.by_type('image').to_json};") +
+            stylesheet_link_tag('jstoolbar')
+        end
+        @heads_for_wiki_formatter_included = true
+      end
     end
   end
 end
